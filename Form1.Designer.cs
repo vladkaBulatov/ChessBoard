e39e196f4412281fc7c8b313400819c7fad2fc4f
Влад Булатов <vladka709470@gmail.com>
2017-04-22 09:46:31 +0200

    new project

---
+++ Form1.Designer.cs
@@ -0,0 +1,66 @@
+﻿namespace Шахматная_доска
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.picture = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picture)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picture
+            // 
+            this.picture.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.picture.Location = new System.Drawing.Point(-2, -2);
+            this.picture.Name = "picture";
+            this.picture.Size = new System.Drawing.Size(566, 483);
+            this.picture.TabIndex = 0;
+            this.picture.TabStop = false;
+            this.picture.Click += new System.EventHandler(this.picture_Click);
+            this.picture.Paint += new System.Windows.Forms.PaintEventHandler(this.picture_Paint);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(704, 482);
+            this.Controls.Add(this.picture);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picture)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picture;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Шахматная_доска
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+            //DrawEllipse(486, 158, 40, 40);
+            Draw();
+        }
+        
+        private void DrawEllipse(int x, int y, int rx, int ry)
+        {
+            Bitmap bmp1 = new Bitmap(picture.Width, picture.Height);
+            Pen pen = new Pen(Color.Black);
+            Graphics graph1 = Graphics.FromImage(bmp1);
+            Rectangle rect = new Rectangle(x, y, rx, ry);
+            graph1.DrawEllipse(pen, rect);
+
+            picture.Image = bmp1;
+
+            
+        }
+
+        public void Draw()
+        {
+            SolidBrush solidBrush = new SolidBrush(Color.Black);
+            //Color.FromArgb(255, 255, 0, 0));
+
+
+            Bitmap bmp = new Bitmap(picture.Width, picture.Height);
+            Graphics graph = Graphics.FromImage(bmp);
+            Pen pen = new Pen(Color.Black);
+            // шашка
+
+            graph.FillRectangle(solidBrush, 10, 10, 100, 100);
+            //оконтовка
+            graph.DrawLine(pen, 10, 10, 446, 10);
+            graph.DrawLine(pen, 10, 10, 10, 446);
+            graph.DrawLine(pen, 10, 446, 446, 446);
+            graph.DrawLine(pen, 446, 10, 446, 446);
+            //горизонтальные линии
+            graph.DrawLine(pen, 40, 40, 416, 40);
+            graph.DrawLine(pen, 40, 87, 416, 87);
+            graph.DrawLine(pen, 40, 134, 416, 134);
+            graph.DrawLine(pen, 40, 181, 416, 181);
+            graph.DrawLine(pen, 40, 228, 416, 228);
+            graph.DrawLine(pen, 40, 275, 416, 275);
+            graph.DrawLine(pen, 40, 322, 416, 322);
+            graph.DrawLine(pen, 40, 369, 416, 369);
+            graph.DrawLine(pen, 40, 416, 416, 416);
+            //вертикальные линии
+            graph.DrawLine(pen, 40, 40, 40, 416);
+            graph.DrawLine(pen, 87, 40, 87, 416);
+            graph.DrawLine(pen, 134, 40, 134, 416);
+            graph.DrawLine(pen, 181, 40, 181, 416);
+            graph.DrawLine(pen, 228, 40, 228, 416);
+            graph.DrawLine(pen, 275, 40, 275, 416);
+            graph.DrawLine(pen, 322, 40, 322, 416);
+            graph.DrawLine(pen, 369, 40, 369, 416);
+            graph.DrawLine(pen, 416, 40, 416, 416);
+
+            
+            
+            
+
+            picture.Image = bmp;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void picture_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void picture_Paint(object sender, PaintEventArgs e)
+        {
+            
+        }
+    }
+}
