4c30b11e916416582da0065c1db0d472c3aba121
Влад Булатов <vladka709470@gmail.com>
2017-04-30 16:25:07 +0200

    Добавил классы

---
+++ Checker.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace Шахматная_доска
+{
+    class Checker
+    {
+        Color Color;
+        int Diametr;
+        int X;
+        int Y;
+    }
+}
